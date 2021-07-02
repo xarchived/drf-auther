42e9d4345e2a59bb7c2c93603a54fe4bfebf0ac0
Xurvan <xurvan@tuta.io>
2021-07-02 18:48:42 +0430

    Change import convention

--- auther/auth.py
+++ auther/auth.py
@@ -5,16 +5,17 @@ from redisary import Redisary
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.request import Request
 
-from auther import models, settings
+from auther.models import User, Session
+from auther.settings import REDIS_DB, MAX_SESSIONS, TOKEN_NAME
 from auther.utils import generate_token, check_password
 
-tokens = Redisary(db=settings.REDIS_DB)
+tokens = Redisary(db=REDIS_DB)
 
 
-def authenticate(username: str, password: str) -> models.User:
+def authenticate(username: str, password: str) -> User:
     try:
-        user = models.User.objects.get(username=username)
-    except models.User.DoesNotExist:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
         raise AuthenticationFailed('Username and/or password is wrong')
 
     if user.deleted_at:
@@ -26,8 +27,8 @@ def authenticate(username: str, password: str) -> models.User:
     if user.expire and user.expire < timezone.now():
         raise AuthenticationFailed('Account has expire')
 
-    session = models.Session.objects.filter(user_id=user.id)
-    if len(session) > settings.MAX_SESSIONS:
+    session = Session.objects.filter(user_id=user.id)
+    if len(session) > MAX_SESSIONS:
         raise AuthenticationFailed('Maximum number of sessions exceeded')
 
     if check_password(password, user.password):
@@ -36,10 +37,10 @@ def authenticate(username: str, password: str) -> models.User:
     raise AuthenticationFailed('Username and/or password is wrong')
 
 
-def login(user: models.User, user_agent: str) -> str:
+def login(user: User, user_agent: str) -> str:
     token = generate_token()
 
-    session = models.Session(token=token, user=user, user_agent=user_agent)
+    session = Session(token=token, user=user, user_agent=user_agent)
     session.save()
 
     tokens[token] = json.dumps(user.as_simple_dict)
@@ -49,7 +50,7 @@ def login(user: models.User, user_agent: str) -> str:
 
 # noinspection PyProtectedMember
 def logout(request: Request) -> None:
-    token_name = settings.TOKEN_NAME
+    token_name = TOKEN_NAME
 
     if token_name in request._request.COOKIES:
         del tokens[request._request.COOKIES[token_name]]
