19b9af3c029e02545a57d87c127d7df37ac7a1ec
Xurvan <xurvan@tutanota.com>
2021-08-20 15:36:24 +0430

    Replace HTTP status code with DRF statics

--- auther/exceptions.py
+++ auther/exceptions.py
@@ -1,12 +1,13 @@
 from django.utils.translation import gettext_lazy as _
+from rest_framework import status
 from rest_framework.exceptions import APIException
 
 
 class PrivilegeError(APIException):
-    status_code = 403
+    status_code = status.HTTP_403_FORBIDDEN
     default_detail = _('privilege is not suffice')
 
 
 class AlreadySet(APIException):
-    status_code = 409
-    default_detail = _('Already exists')
+    status_code = status.HTTP_409_CONFLICT
+    default_detail = _('already exists')
