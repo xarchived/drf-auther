5df23f56d678e0392cf61eda107a638d675abffe
Xurvan <xurvan@tutanota.com>
2021-08-26 16:17:48 +0430

    Using "DetailRouter"

--- auther/urls.py
+++ auther/urls.py
@@ -11,6 +11,7 @@ from auther.views import (
     SendOtpView,
     MeViewSet,
 )
+from fancy.routers import DetailRouter
 
 router = SimpleRouter()
 router.register(r'perms', PermViewSet)
@@ -18,9 +19,12 @@ router.register(r'roles', RoleViewSet)
 router.register(r'domains', DomainViewSet)
 router.register(r'users', UserViewSet)
 
+detail_router = DetailRouter()
+detail_router.register(r'self', MeViewSet)
+
 urlpatterns = [
     path('', include(router.urls)),
-    path('self/', MeViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'update'})),
+    path('', include(detail_router.urls)),
     path('login/', LoginView.as_view()),
     path('logout/', LogoutView.as_view()),
     path('send_otp/', SendOtpView.as_view()),
